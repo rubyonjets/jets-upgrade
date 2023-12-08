fda70ca7a9966d10159f975b33db0abd9bc1254b
Tung Nguyen <tongueroo@gmail.com>
2023-12-08 14:35:26 +0000

    rewrite controller use respond_to for destroy action

--- lib/jets_upgrade/rewrite/controller.rb
+++ lib/jets_upgrade/rewrite/controller.rb
@@ -13,16 +13,40 @@ module JetsUpgrade::Rewrite
     end
 
     def content
+      within_destroy = false
       modified_lines = lines.map do |line|
         if line =~ /\s{2,}def delete$/
           line = line.sub('delete', 'destroy')
+          within_destroy = true
         end
+        if within_destroy
+          if line =~ /\s{2,}redirect_to (\w+)$/
+            location_method = $1 # IE: posts_path
+            line = respond_to_code(location_method)
+          end
+        end
+        if within_destroy && line =~ /\s{2,}end$/
+          within_destroy = false
+        end
+
         if line =~ /before_action :/ && line =~ /delete/
           line = line.sub('delete', 'destroy')
         end
         line
       end
       modified_lines.join('')
+    end
+
+    def respond_to_code(location_method)
+      code = <<~EOL
+        respond_to do |format|
+          format.html { redirect_to #{location_method} }
+          format.js   { render json: { location: #{location_method} } }
+        end
+      EOL
+      lines = code.split("\n")
+      # add 4 spaces to each line
+      lines.map! { |line| "    #{line}" }.join("\n") + "\n"
     end
   end
 end
